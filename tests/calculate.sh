288fdf8c9cf02363e099acc9254727532644597f
Gavin Peng <gpeng@oicr.on.ca>
2024-10-09 14:59:51 -0400

    simplify calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -5,28 +5,5 @@ set -o pipefail
 
 cd "$1"
 
-# Function to process _dqsummary.html file
-process_dqsummary() {
-    local file="$1"
-    local temp_file=$(mktemp)
-    
-    # Remove the line containing "Timestamp:" and save to temp file
-    grep -v "Timestamp:" "$file" > "$temp_file"
-    
-    # Generate MD5 sum of the temp file and print with filename
-    md5sum "$temp_file" | awk -v fname="$file" '{print $1 "  " fname}'
-    
-    # Clean up
-    rm "$temp_file"
-}
-
-# Use process substitution to combine and sort the output
-{
-    # Process Summary.csv files
-    find . -name "*_Summary.csv" -type f | xargs md5sum
-
-    # Process _dqsummary.html files
-    find . -name "*_dqsummary.html" -type f | while read -r file; do
-        process_dqsummary "$file"
-    done
-} | sort
+ls | sed 's/.*\.//' | sort | uniq -c
+find . -name "*_Summary.csv" -type f | xargs md5sum
