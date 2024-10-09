f2c85dc330f9d32553a22b62a7e7b23e02bfddf2
Gavin Peng <gpeng@oicr.on.ca>
2024-10-09 10:27:07 -0400

    modify calculate.sh to ignore timestamp in dqsummary file

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -3,7 +3,27 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
-cd $1
+cd "$1"
 
-find -name *_Summary.csv | xargs md5sum 
-find -name *_dqsummary.html | xargs md5sum
+# Function to process _dqsummary.html file
+process_dqsummary() {
+    local file="$1"
+    local temp_file=$(mktemp)
+    
+    # Remove the line containing "Timestamp:" and save to temp file
+    grep -v "Timestamp:" "$file" > "$temp_file"
+    
+    # Generate MD5 sum of the temp file
+    md5sum "$temp_file" | awk -v fname="$file" '{print $1 "  " fname}' 
+    
+    # Clean up
+    rm "$temp_file"
+}
+
+# Process Summary.csv files
+find . -name "*_Summary.csv" -type f | xargs md5sum
+
+# Process _dqsummary.html files
+find . -name "*_dqsummary.html" -type f | while read -r file; do
+    process_dqsummary "$file"
+done
